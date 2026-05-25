a62fe929e31964a966acfd973058f9e1306bd78c
Md Mustaqeem <mdmustaqeem820@gmail.com>
2026-05-25 23:00:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(square => {
+
+	square.addEventListener("mouseover", () => {
+
+		squares.forEach(otherSquare => {
+
+			if (otherSquare !== square) {
+				otherSquare.style.backgroundColor = "#6F4E37";
+			}
+
+		});
+
+	});
+
+	square.addEventListener("mouseout", () => {
+
+		squares.forEach(otherSquare => {
+			otherSquare.style.backgroundColor = "#E6E6FA";
+		});
+
+	});
+
+});
